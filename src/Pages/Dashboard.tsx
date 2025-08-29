12ae7bb2ec228d7315f0ad8d37a07cf71145e92c
prime <primetechnologies02@gmail.com>
2025-08-29 18:09:15 +0100

    changes

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,106 +1,106 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "@/lib/store";
-import { CountDown, NextCourse } from "@/components/SliderComponents";
-import heroImg from "../assets/images/hero.jpg";
-import HomeSlider from "../components/HomeSlider";
-import TimeTable from "@/components/TimeTable";
-import CalendarWithNotes from "@/components/CalendarWithTasks";
-import ClashedCourses from "@/components/ClashedCourses";
-import { getCourses } from "@/lib/slice/CourseSlice";
-import TableLoader from "@/lib/utils/tableLoader";
-import ReportBugModal from "@/components/modals/reportBugModal";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '@/lib/store';
+import { CountDown, NextCourse } from '@/components/SliderComponents';
+import heroImg from '../assets/images/hero.jpg';
+import HomeSlider from '../components/HomeSlider';
+import TimeTable from '@/components/TimeTable';
+import CalendarWithNotes from '@/components/CalendarWithTasks';
+import ClashedCourses from '@/components/ClashedCourses';
+import { getCourses } from '@/lib/slice/CourseSlice';
+import TableLoader from '@/lib/utils/tableLoader';
+import ReportBugModal from '@/components/modals/ReportBugModal';
 
 const Dashboard = () => {
-	const dispatch = useDispatch<AppDispatch>();
-	// state to handle the report bug modal
-	const [open, setOpen] = useState<boolean>(false);
+  const dispatch = useDispatch<AppDispatch>();
+  // state to handle the report bug modal
+  const [open, setOpen] = useState<boolean>(false);
 
-	// get the reload function from the store in order to get the lastes courses data
-	const { reload, data, isLoading } = useSelector((state: any) => state.course);
+  // get the reload function from the store in order to get the lastes courses data
+  const { reload, data, isLoading } = useSelector((state: any) => state.course);
 
-	useEffect(() => {
-		dispatch(getCourses());
-	}, [reload, dispatch]);
+  useEffect(() => {
+    dispatch(getCourses());
+  }, [reload, dispatch]);
 
-	return (
-		<main className="my-2">
-			<div
-				className="relative w-full h-[400px]"
-				style={{
-					backgroundImage: `url(${heroImg})`,
-					backgroundSize: "cover",
-				}}
-			>
-				<div
-					className="absolute top-0 left-0 w-full h-full flex justify-center items-center lg:justify-end"
-					style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-				>
-					<div className="hidden text-center leading-tight uppercase font-extrabold tracking-tight mr-16 lg:block">
-						<h2 className="text-sm text-gray-500">It's Time To</h2>
-						<h1 className="text-5xl sm:text-6xl text-green-500">Organize</h1>
-						<h1 className="text-5xl sm:text-6xl text-gray-900">Plan</h1>
-						<h1 className="text-3xl sm:text-4xl text-green-500">
-							Your Timetable
-						</h1>
-						<p className="text-sm mt-2 text-gray-400 lowercase italic">
-							with ease & clarity!
-						</p>
-					</div>
-					<div className="text-center leading-tight uppercase font-extrabold tracking-tight lg:hidden">
-						<h2 className="text-sm text-gray-300">It's Time To</h2>
-						<h1 className="text-5xl sm:text-6xl text-green-500">Organize</h1>
-						<h1 className="text-5xl sm:text-6xl text-gray-400">Plan</h1>
-						<h1 className="text-3xl sm:text-4xl text-green-500">
-							Your Timetable
-						</h1>
-						<p className="text-md mt-2 text-gray-200 lowercase italic">
-							with ease & clarity!
-						</p>
-					</div>
-				</div>
-			</div>
-			<HomeSlider />
-			<div className="w-11/12 mx-auto lg:hidden">
-				<div>
-					<TimeTable />
-				</div>
-			</div>
-			<div className="hidden w-11/12 mx-auto mt-12 grid-cols-4 lg:grid gap-4">
-				<div className="col-span-3">
-					<div className="flex items-center gap-4 mb-8">
-						<div className="flex flex-col justify-center items w-full h-full lg:h-48 lg:gap-4">
-							{isLoading ? (
-								<div className="w-full flex items-center justify-center">
-									<TableLoader />
-								</div>
-							) : (
-								<NextCourse data={data} />
-							)}
-						</div>
-						<CountDown />
-					</div>
-					<div>
-						<TimeTable />
-					</div>
-				</div>
-				<div className="col-span-1">
-					<div>
-						<ClashedCourses />
-					</div>
-					<CalendarWithNotes />
-				</div>
-			</div>
+  return (
+    <main className="my-2">
+      <div
+        className="relative w-full h-[400px]"
+        style={{
+          backgroundImage: `url(${heroImg})`,
+          backgroundSize: 'cover',
+        }}
+      >
+        <div
+          className="absolute top-0 left-0 w-full h-full flex justify-center items-center lg:justify-end"
+          style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+        >
+          <div className="hidden text-center leading-tight uppercase font-extrabold tracking-tight mr-16 lg:block">
+            <h2 className="text-sm text-gray-500">It's Time To</h2>
+            <h1 className="text-5xl sm:text-6xl text-green-500">Organize</h1>
+            <h1 className="text-5xl sm:text-6xl text-gray-900">Plan</h1>
+            <h1 className="text-3xl sm:text-4xl text-green-500">
+              Your Timetable
+            </h1>
+            <p className="text-sm mt-2 text-gray-400 lowercase italic">
+              with ease & clarity!
+            </p>
+          </div>
+          <div className="text-center leading-tight uppercase font-extrabold tracking-tight lg:hidden">
+            <h2 className="text-sm text-gray-300">It's Time To</h2>
+            <h1 className="text-5xl sm:text-6xl text-green-500">Organize</h1>
+            <h1 className="text-5xl sm:text-6xl text-gray-400">Plan</h1>
+            <h1 className="text-3xl sm:text-4xl text-green-500">
+              Your Timetable
+            </h1>
+            <p className="text-md mt-2 text-gray-200 lowercase italic">
+              with ease & clarity!
+            </p>
+          </div>
+        </div>
+      </div>
+      <HomeSlider />
+      <div className="w-11/12 mx-auto lg:hidden">
+        <div>
+          <TimeTable />
+        </div>
+      </div>
+      <div className="hidden w-11/12 mx-auto mt-12 grid-cols-4 lg:grid gap-4">
+        <div className="col-span-3">
+          <div className="flex items-center gap-4 mb-8">
+            <div className="flex flex-col justify-center items w-full h-full lg:h-48 lg:gap-4">
+              {isLoading ? (
+                <div className="w-full flex items-center justify-center">
+                  <TableLoader />
+                </div>
+              ) : (
+                <NextCourse data={data} />
+              )}
+            </div>
+            <CountDown />
+          </div>
+          <div>
+            <TimeTable />
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div>
+            <ClashedCourses />
+          </div>
+          <CalendarWithNotes />
+        </div>
+      </div>
 
-			<button
-				onClick={() => setOpen(true)}
-				className="fixed bottom-10 left-10 font-semibold glassmorphism bg-[rgb(255,255,255,0.05)] border-[rgb(234,234,234)] shadow-[0_4px_30px_rgba(80,80,80,0.1)] rounded-xl py-2 px-4 font-inter text-xs text-green-400 border-1 dark:bg-[rgba(255,255,255,0.05)] dark:border-[rgb(68,68,68)] cursor-pointer duration-500 hover:text-green-600"
-			>
-				suggest updates/report bug
-			</button>
-			{open && <ReportBugModal open={open} setOpen={setOpen} />}
-		</main>
-	);
+      <button
+        onClick={() => setOpen(true)}
+        className="fixed bottom-10 left-10 font-semibold glassmorphism bg-[rgb(255,255,255,0.05)] border-[rgb(234,234,234)] shadow-[0_4px_30px_rgba(80,80,80,0.1)] rounded-xl py-2 px-4 font-inter text-xs text-green-400 border-1 dark:bg-[rgba(255,255,255,0.05)] dark:border-[rgb(68,68,68)] cursor-pointer duration-500 hover:text-green-600"
+      >
+        suggest updates/report bug
+      </button>
+      {open && <ReportBugModal open={open} setOpen={setOpen} />}
+    </main>
+  );
 };
 
 export default Dashboard;
